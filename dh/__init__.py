78847d2e14384b5b54ad5d35b47f97f4afb6ded1
athe1st666 <74457152+athe1st666@users.noreply.github.com>
2021-04-19 20:35:08 +0800

    git add <comms.py>

--- dh/__init__.py
+++ dh/__init__.py
@@ -22,15 +22,87 @@ prime = read_hex(raw_prime)
 
 # Project TODO: write the appropriate code to perform DH key exchange
 
-def create_dh_key() -> Tuple[int, int]:
+#def create_dh_key() -> Tuple[int, int]:
     # Creates a Diffie-Hellman key
     # Returns (public, private)
-    a = random.randint(0, int(2**8))
-    return (a, a)
+   # a = random.randint(0, int(2**8))
+    #return (a, a)
 
-def calculate_dh_secret(their_public: int, my_private: int) -> bytes:
+
+#implementation of password exchange process
+#server fetching p (code specific to how to do it on the server)
+#Large prime number i.e. pprime
+#Given that the example code lacks the process of interaction between the server and the individual bots, it is better to write it as you understand it
+#examples point to mostly summary algorithms, not complete DH implementation, so re-represented in the process, but may combine with problems
+# As I understand it, the DH process is performed before all communication, and only after the DH verification is completed does the later communication begin
+
+p = prime
+
+# for bot1 Get calculated numbers
+def get_calculation（ p ,a , X ):
+	Y = pow( a , X , p )
+	return Y
+
+#ExchangeSecretKey
+def exchange_key( X , Y , p ):
+	key = pow( Y , X , p )
+	return key
+
+#Obtain the original roots a,b
+#a,b is a positive integer less than p and is required to be large, here, only a larger number is left
+#In practice you can do a test, e.g. greater than 100 bits to be retained for security, e.g. test greater than 2**20
+def get_generator(p):
+	a = 2
+	list = []
+	while a < p:		
+		flag = 1
+		while flag != p:
+			if (a ** flag) % p == 1:
+				break
+			flag += 1
+		if flag == (p - 1):
+			list.append(a)	
+		a += 1
+	return list
+
+def get_calculation(p,a,X):
+	Y = pow(a,X,p)
+	return Y
+
+def get_key(X,Y,p):
+	key = pow(Y,X,p)
+	return key
+
+list = get_generator(p)
+	print(str(p) + ' 的一个原根为：', end = '')
+	print(list[-1])
+	print('------------------------------------------------------------------------------')
+	
+#A's private key
+	XA = random.randint(0, p-1)
+
+#B's private key
+	XB = random.randint(0, p-1)
+
+#Number of calculations for A
+	YA = get_calculation(p, int(list[-1]), XA)
+	
+#Number of calculations for B
+	YB = get_calculation(p, int(list[-1]), XB)
+
+
+#A and B's private keys
+key_B = get_key(XB, YA,p)
+key_A = get_key(XA, YB,p)
+	
+# Communication conditions, server confirms key
+def judge_key():
+	if key_A == key_B return True
+
+
+#def calculate_dh_secret(their_public: int, my_private: int) -> bytes:
     # Calculate the shared secret
-    shared_secret = their_public * my_private
+    #shared_secret = their_public * my_private
 
     # Hash the value so that:
     # (a) There's no bias in the bits of the output
@@ -38,5 +110,5 @@ def calculate_dh_secret(their_public: int, my_private: int) -> bytes:
     # (b) We can convert to raw bytes easily
     # (c) We could add additional information if we wanted
     # Feel free to change SHA256 to a different value if more appropriate
-    shared_hash = SHA256.new(str(shared_secret).encode()).digest()
-    return shared_hash
+    #shared_hash = SHA256.new(str(shared_secret).encode()).digest()
+    #return shared_hash
